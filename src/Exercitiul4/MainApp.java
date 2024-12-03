3837eea77ae4924b6b4c91bb0205ad22ea0c5aef
IrasogAlex <irasog.alexandru@gmail.com>
2024-12-03 18:36:40 +0200

    Tema1 primul commit

---
+++ src/Exercitiul4/MainApp.java
@@ -0,0 +1,28 @@
+package Exercitiul4;
+
+import java.util.Random;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int a = random.nextInt(30) + 1;
+        int b = random.nextInt(30) + 1;
+
+        System.out.println("Numere generate: " + a + " și " + b);
+
+        int cmmdc = calculeazaCmmdc(a, b);
+
+        System.out.println("CMMDC al numerelor " + a + " și " + b + " este: " + cmmdc);
+    }
+
+    public static int calculeazaCmmdc(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
